aaa008bb2128d569dc6385d5405b2902166376b8
T2 <t2.tide@gmail.com>
2017-07-28 19:11:11 +0900

    add constants file in shared.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { PAGE_TITLE, STATUS, KEY_CODE } from './shared/constants';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  readonly KEY_CODE = KEY_CODE;
+  readonly STATUS = STATUS;
+
+  title = PAGE_TITLE.HOME;
+  value: any = '';
+  message: string;
+  
+  onKeyDown(event): boolean {
+    console.log(event);
+    if (event.keyCode === KEY_CODE.KEY_0) {
+      this.message = this.STATUS.OPEN;
+      return true;
+    } else {
+      this.message = this.STATUS.CLOSE;
+      return false;
+    }
+  }
 }
---
+++ src/app/shared/constants.ts
@@ -0,0 +1,41 @@
+export const PAGE_TITLE = {
+  HOME: 'app',
+};
+
+export type STATUS =
+  'open' | 'close';
+export namespace STATUS {
+  export const OPEN: STATUS = 'open';
+  export const CLOSE: STATUS = 'close';
+}
+
+export enum KEY_CODE {
+  KEY_TAB = 9,
+  KEY_ENTER = 13,
+  KEY_SHIFT = 16,
+  KEY_ARROW_LEFT = 37,
+  KEY_ARROW_UP = 38,
+  KEY_ARROW_RIGHT = 39,
+  KEY_ARROW_DOWN = 40,
+  KEY_0 = 48,
+  KEY_1 = 49,
+  KEY_2 = 50,
+  KEY_3 = 51,
+  KEY_4 = 52,
+  KEY_5 = 53,
+  KEY_6 = 54,
+  KEY_7 = 55,
+  KEY_8 = 56,
+  KEY_9 = 57,
+  KEY_E = 69,
+  NUMPAD_0 = 96,
+  NUMPAD_1 = 97,
+  NUMPAD_2 = 98,
+  NUMPAD_3 = 99,
+  NUMPAD_4 = 100,
+  NUMPAD_5 = 101,
+  NUMPAD_6 = 102,
+  NUMPAD_7 = 103,
+  NUMPAD_8 = 104,
+  NUMPAD_9 = 105,
+};
